01e20db8b05aea313c749b1d22b3f95ba660e273
loutianchi <loutianchi@sjtu.edu.cn>
2024-03-11 00:15:57 +0800

    finish hw2

--- Advanced-Data-Structure/hw2-Bloom_Filter/main.cc
+++ Advanced-Data-Structure/hw2-Bloom_Filter/main.cc
@@ -1,21 +1,57 @@
 #include <iostream>
 #include "MurmurHash3.h"
 
+#include <map>
+#include <cstring>
+#include <iomanip>
+
 int main() {
-    uint64_t key = 522123456789;
+    std::map<uint64_t, uint64_t> mp;
+    uint64_t key;
     uint64_t hash[2] = {0};
-
-    MurmurHash3_x64_128(&key, sizeof(key), 1, hash);
-
-    std::cout << "Hash value: ";
-    for(int i = 0; i < 2; i++) {
-        std::cout << hash[i] << " ";
-    }
-    std::cout << std::endl;
-    //mod m-1
-    //only use hash[1] 
-    int m = 100;
-    std::cout << hash[1]%(m-1) << " ";
     
+    int mDn ,k ,m;
+    std::cin >> mDn >> k;
+    int totN = 0, totW = 0;
+
+    std::cout << std::fixed << std::setprecision(5);
+
+    for (int n = 50; n < 10000; n += 50) {
+        m = n * mDn;
+        totN += n;
+
+        bool *flag;
+        flag = new bool[m];
+        for (int i = 0; i < m; i++)
+            flag[i] = false;
+
+        for (int i = 0; i < n; i++) {
+            key = i;
+            for (int j = 1; j <= k; j++) {
+                MurmurHash3_x64_128(&key, sizeof(key), j, hash);
+                flag[hash[0] % m] = true;
+            }
+        }
+            
+        bool isExi;
+        int totWrong = 0;
+        for (int i = n + 1; i <= n * 2; i++) {
+            key = i;
+            isExi = true;
+            for (int j = 1; j <= k; j++) {
+                MurmurHash3_x64_128(&key, sizeof(key), j, hash);
+                if (!flag[hash[1] % m]) isExi = false;
+            }
+            if (isExi)
+                totWrong++;
+        }
+        totW += totWrong;
+        //std::cout << totWrong * 1.0 / n << std::endl;
+
+        delete []flag;
+    }
+    std::cout << std::endl << totW << " " << totN << std::endl;
+    std::cout << std::endl << totW * 1.0 / totN << std::endl;
+
     return 0;
 }
